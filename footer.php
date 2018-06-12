24796e81a26f9c918c84babab139f8554b0f9bbe
mgashini95 <mganisansala@gmail.com>
2018-06-12 15:22:27 +0530

    #3 Add current design

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer>
+    <div id="csi-footer" class="csi-footer">
+        <div class="csi-inner">
+            <div class="footer-top-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="footer-top">
+                                <a class="footer-logo" href="index.html"><img src="assets/img/logo.png" alt="logo"/></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3">
+                            <div class="single">
+                                <h3 class="title">Contact</h3>
+                                <p>Dewamiththa Rd, Galle, Sri Lanka</p>
+                                <p>[email protected]</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-3">
+                            <div class="single">
+                                <h3 class="title">Book A Table</h3>
+                                <p>pellentesque vehicula. Aliquam turpis justo, mattis id neque</p>
+                                <p>Mobile: (+94) 91 312 7744</p>
+                            </div>
+                        </div> 
+                        <div class="col-xs-12 col-sm-3">
+                            <div class="single">
+                                <h3 class="title">Opening Time</h3>
+                                <p>Mon - Fri 7:00 - 22:00 clock </p>
+                                <p>Sat - Sun 8:00 - 23:00 clock </p>
+                            </div>
+                        </div> 
+                        <div class="col-xs-12 col-sm-3">
+                            <div class="single">
+                                <h3 class="title">Social Links</h3>
+                                <ul class="list-inline footer-social">
+                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-facebook-f" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="csi-footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> all rights reserved by <a href="http://www.sublime.lk/">Sublime</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
